fdf07b28842514016401f1b082be58c36054bbad
rfoxmich <fox@nscl.msu.edu>
2022-05-20 11:00:33 -0400

    Excersize allocation and usage

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
-pub mod  portpool;
+mod portpool;
+use portpool::ports;
 fn main() {
-    println!("Hello, world!");
-    let mut pool = portpool::ports::PortPool::new(30000, 1000);
+    
+    let mut pool = ports::PortPool::new(30000, 1000);
 
     let p = pool.allocate("RingMaster", "fox").expect("Allocation failed");
     println!("{}", p);
+
+    let usage = pool.usage();
+    println!("OK {}", usage.len());
+    for p  in usage {
+        println!("{}", p);
+    }
 }
