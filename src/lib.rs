92d16f2e6fd98b8c0df576447b32d7138989fb62
rfoxmich <fox@nscl.msu.edu>
2022-07-25 09:35:24 -0400

    Start writing the resopnder module

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,2 @@
 pub mod portpool;
+pub mod responder;
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,39 @@
 mod portpool;
+mod responder;
 use portpool::ports;
 fn main() {
-    
     let mut pool = ports::PortPool::new(30000, 1000);
 
     // Allocation patterns:
 
-    let p1 = pool.allocate("RingMaster", "fox").expect("Allocation failed");
+    let p1 = pool
+        .allocate("RingMaster", "fox")
+        .expect("Allocation failed");
     let p2 = pool.allocate("Readout", "fox").expect("Allocation failed");
 
     print_usage(&pool);
 
     // Free:
-    
+
     pool.free(p1.port()).unwrap();
     print_usage(&pool);
 
     pool.free(p2.port()).unwrap();
     print_usage(&pool);
-    
 }
-fn print_usage(pool : &ports::PortPool) {
+fn print_usage(pool: &ports::PortPool) {
     println!("{}", get_usage(pool));
 }
 
-fn get_usage(pool : &ports::PortPool) -> String{
+fn get_usage(pool: &ports::PortPool) -> String {
     let usage = pool.usage();
-    let mut result =  String::new();
+    let mut result = String::new();
     result += format!("OK {}\n", usage.len()).as_str();
-    for p  in &usage {
+    for p in &usage {
         result += format!("{}\n", p).as_str();
     }
     if usage.len() > 0 {
-        result.pop();             // Get rid of extra trailing \n
+        result.pop(); // Get rid of extra trailing \n
     }
     result
 }
